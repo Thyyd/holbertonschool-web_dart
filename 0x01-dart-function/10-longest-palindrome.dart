e303cc9c288af54bd934f184e7b507b3dbbe1aae
Thyyd <roastingblade@gmail.com>
2026-07-30 02:46:35 +0200

    Ajout du fichier de la task 10 : Fonction longestPalindrome

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -4,6 +4,7 @@ String longestPalindrome(String s) {
   String longest = '';
   String extracted = '';
 
+  // Génération de toutes les sous-chaînes possibles pour vérifier s'il y'a des palindromes et ne renvoyer que le plus long.
   for (int i = 0; i < s.length; i++) {
     for (int j = i+1; j <= s.length; j++) {
       extracted = s.substring(i, j);
